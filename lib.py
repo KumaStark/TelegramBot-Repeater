87c06989a8bcd3a6c0a6a81134d40d678db0cb80
KumaStark <elastic48@msn.com>
2023-05-22 16:29:49 +0800

    fix custom regular expression

--- lib.py
+++ lib.py
@@ -7,6 +7,6 @@ def read_yaml_config(file_path):
         return config
 
 def kuma_custom_check(text):
-    pattern = r'熊熊.*?["“]([^"”]+)["”]'
+    pattern = r'[熊熊|熊熊|气泡|氣泡|泡泡].*?["“]([^"”]+)["”]'
     result = re.findall(pattern, text)
     return result
